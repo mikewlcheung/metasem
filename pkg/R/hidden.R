2ec6651f6bf296451dbc0c71e6ef59227f88ae94
mikewlcheung <mikewlcheung@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-10-10 03:28:55 +0000

    Added new functions on matrices
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/metasem@37 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/hidden.R
+++ pkg/R/hidden.R
@@ -81,28 +81,3 @@
 ## http://www.math.yorku.ca/Who/Faculty/Monette/pub/stmp/0827.html
 ## http://tolstoy.newcastle.edu.au/R/help/04/05/1322.html
 ## It works for character matrices.
-.bdiag <- function(x){
-  if(!is.list(x)) stop("\"x\" not a list")
-  n <- length(x)
-  if(n==0) return(NULL)
-  x <- lapply(x, function(y) if(length(y)) as.matrix(y) else stop("Zero-length component in x"))
-  d <- array(unlist(lapply(x, dim)), c(2, n))
-  rr <- d[1,]
-  cc <- d[2,]
-  rsum <- sum(rr)
-  csum <- sum(cc)
-  out <- array(0, c(rsum, csum))
-  ind <- array(0, c(4, n))
-  rcum <- cumsum(rr)
-  ccum <- cumsum(cc)
-  ind[1,-1] <- rcum[-n]
-  ind[2,] <- rcum
-  ind[3,-1] <- ccum[-n]
-  ind[4,] <- ccum
-  imat <- array(1:(rsum * csum), c(rsum, csum))
-  iuse <- apply(ind, 2, function(y, imat) imat[(y[1]+1):y[2],
-  (y[3]+1):y[4]], imat=imat)
-  iuse <- as.vector(unlist(iuse))
-  out[iuse] <- unlist(x)
-  return(out)
-} 
--- pkg/R/matrix2bdiag.R
+++ pkg/R/matrix2bdiag.R
@@ -1,6 +1,6 @@
 matrix2bdiag <- function(x, ...) {
   tmp <- split(as.matrix(x), row(x))
-  # Use .bdiag() as it handles string matrices
-  out <- .bdiag(lapply(tmp, vec2symMat, ...))
+  # Use bdiagMat() to handle string matrices
+  out <- bdiagMat(lapply(tmp, vec2symMat, ...))
   as.matrix(out)
 }
