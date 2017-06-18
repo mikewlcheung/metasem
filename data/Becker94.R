39c5487ce1d1a39c258dc710db316586a20a9323
Mike Cheung <mikewlcheung@nus.edu.sg>
2017-06-18 22:33:55 +0800

    Changed the variable names in Becker94

--- data/Becker94.R
+++ data/Becker94.R
@@ -1,35 +1,35 @@
 Becker94 <-
 structure(list(data = structure(list(`Becker (1978) Females` = structure(c(1, 
 0.47, -0.21, 0.47, 1, -0.15, -0.21, -0.15, 1), .Dim = c(3L, 3L
-), .Dimnames = list(c("SAT (Math)", "Spatial", "SAT (Verbal)"
-), c("SAT (Math)", "Spatial", "SAT (Verbal)"))), `Becker (1978) Males` = structure(c(1, 
+), .Dimnames = list(c("SAT_Math", "Spatial", "SAT_Verbal"
+), c("SAT_Math", "Spatial", "SAT_Verbal"))), `Becker (1978) Males` = structure(c(1, 
 0.28, 0.19, 0.28, 1, 0.18, 0.19, 0.18, 1), .Dim = c(3L, 3L), .Dimnames = list(
-    c("SAT (Math)", "Spatial", "SAT (Verbal)"), c("SAT (Math)", 
-    "Spatial", "SAT (Verbal)"))), `Berry (1957) Females` = structure(c(1, 
+    c("SAT_Math", "Spatial", "SAT_Verbal"), c("SAT_Math", 
+    "Spatial", "SAT_Verbal"))), `Berry (1957) Females` = structure(c(1, 
 0.48, 0.41, 0.48, 1, 0.26, 0.41, 0.26, 1), .Dim = c(3L, 3L), .Dimnames = list(
-    c("SAT (Math)", "Spatial", "SAT (Verbal)"), c("SAT (Math)", 
-    "Spatial", "SAT (Verbal)"))), `Berry (1957) Males` = structure(c(1, 
+    c("SAT_Math", "Spatial", "SAT_Verbal"), c("SAT_Math", 
+    "Spatial", "SAT_Verbal"))), `Berry (1957) Males` = structure(c(1, 
 0.37, 0.4, 0.37, 1, 0.27, 0.4, 0.27, 1), .Dim = c(3L, 3L), .Dimnames = list(
-    c("SAT (Math)", "Spatial", "SAT (Verbal)"), c("SAT (Math)", 
-    "Spatial", "SAT (Verbal)"))), `Rosenberg (1981) Females` = structure(c(1, 
+    c("SAT_Math", "Spatial", "SAT_Verbal"), c("SAT_Math", 
+    "Spatial", "SAT_Verbal"))), `Rosenberg (1981) Females` = structure(c(1, 
 0.42, 0.48, 0.42, 1, 0.23, 0.48, 0.23, 1), .Dim = c(3L, 3L), .Dimnames = list(
-    c("SAT (Math)", "Spatial", "SAT (Verbal)"), c("SAT (Math)", 
-    "Spatial", "SAT (Verbal)"))), `Rosenberg (1981) Males` = structure(c(1, 
+    c("SAT_Math", "Spatial", "SAT_Verbal"), c("SAT_Math", 
+    "Spatial", "SAT_Verbal"))), `Rosenberg (1981) Males` = structure(c(1, 
 0.41, 0.74, 0.41, 1, 0.44, 0.74, 0.44, 1), .Dim = c(3L, 3L), .Dimnames = list(
-    c("SAT (Math)", "Spatial", "SAT (Verbal)"), c("SAT (Math)", 
-    "Spatial", "SAT (Verbal)"))), `Weiner A (1984) Females` = structure(c(1, 
+    c("SAT_Math", "Spatial", "SAT_Verbal"), c("SAT_Math", 
+    "Spatial", "SAT_Verbal"))), `Weiner A (1984) Females` = structure(c(1, 
 0.26, 0.72, 0.26, 1, 0.36, 0.72, 0.36, 1), .Dim = c(3L, 3L), .Dimnames = list(
-    c("SAT (Math)", "Spatial", "SAT (Verbal)"), c("SAT (Math)", 
-    "Spatial", "SAT (Verbal)"))), `Weiner A (1984) Males` = structure(c(1, 
+    c("SAT_Math", "Spatial", "SAT_Verbal"), c("SAT_Math", 
+    "Spatial", "SAT_Verbal"))), `Weiner A (1984) Males` = structure(c(1, 
 0.32, 0.52, 0.32, 1, 0.1, 0.52, 0.1, 1), .Dim = c(3L, 3L), .Dimnames = list(
-    c("SAT (Math)", "Spatial", "SAT (Verbal)"), c("SAT (Math)", 
-    "Spatial", "SAT (Verbal)"))), `Weiner B (1984) Females` = structure(c(1, 
+    c("SAT_Math", "Spatial", "SAT_Verbal"), c("SAT_Math", 
+    "Spatial", "SAT_Verbal"))), `Weiner B (1984) Females` = structure(c(1, 
 0.58, 0.64, 0.58, 1, 0.4, 0.64, 0.4, 1), .Dim = c(3L, 3L), .Dimnames = list(
-    c("SAT (Math)", "Spatial", "SAT (Verbal)"), c("SAT (Math)", 
-    "Spatial", "SAT (Verbal)"))), `Weiner B (1984) Males` = structure(c(1, 
+    c("SAT_Math", "Spatial", "SAT_Verbal"), c("SAT_Math", 
+    "Spatial", "SAT_Verbal"))), `Weiner B (1984) Males` = structure(c(1, 
 0.34, 0.28, 0.34, 1, -0.03, 0.28, -0.03, 1), .Dim = c(3L, 3L), .Dimnames = list(
-    c("SAT (Math)", "Spatial", "SAT (Verbal)"), c("SAT (Math)", 
-    "Spatial", "SAT (Verbal)")))), .Names = c("Becker (1978) Females", 
+    c("SAT_Math", "Spatial", "SAT_Verbal"), c("SAT_Math", 
+    "Spatial", "SAT_Verbal")))), .Names = c("Becker (1978) Females", 
 "Becker (1978) Males", "Berry (1957) Females", "Berry (1957) Males", 
 "Rosenberg (1981) Females", "Rosenberg (1981) Males", "Weiner A (1984) Females", 
 "Weiner A (1984) Males", "Weiner B (1984) Females", "Weiner B (1984) Males"
