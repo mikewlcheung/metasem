ea8d8572470b32363c7ac672a87a2d84bc05f40c
Mike Cheung <mikewlcheung@gmail.com>
2018-08-17 14:21:39 +0800

    Added .onLoad()

--- R/zzz.R
+++ R/zzz.R
@@ -25,3 +25,16 @@
     #packageStartupMessage('you may install the "NPSOL" optimizer from the OpenMx website and use it by calling:')
     #packageStartupMessage('mxOption(NULL, "Default optimizer", "NPSOL")')
 }
+
+## Added .onLoad See https://github.com/mikewlcheung/metasem/issues/3 and https://github.com/OpenMx/OpenMx/issues/98
+.onLoad <- function(lib, pkg){
+    mxOption(NULL, "Default optimizer", "SLSQP")
+    mxOption(NULL, "Gradient algorithm", "central")
+    mxOption(NULL, "Optimality tolerance", "6.3e-14")
+    mxOption(NULL, "Gradient iterations", 2)
+
+    packageStartupMessage('"SLSQP" is set as the default optimizer in OpenMx.')
+    packageStartupMessage('mxOption(NULL, "Gradient algorithm") is set at "', mxOption(NULL, "Gradient algorithm"), '".')
+    packageStartupMessage('mxOption(NULL, "Optimality tolerance") is set at "', mxOption(NULL, "Optimality tolerance"), '".')
+    packageStartupMessage('mxOption(NULL, "Gradient iterations") is set at "', mxOption(NULL, "Gradient iterations"), '".')
+}
