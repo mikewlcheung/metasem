57d96422051899e1669e544d19a6d9d30ea07182
Mike Cheung <mikewlcheung@nus.edu.sg>
2019-05-19 16:00:41 +0800

    Added MxModel objects to rerun

--- R/rerun.R
+++ R/rerun.R
@@ -1,10 +1,15 @@
 rerun <- function(object, ...) {
-  if (!is.element(class(object)[1], c("wls", "tssem1FEM", "tssem1REM", "meta", "meta3X", "reml", "tssem1FEM.cluster", "wls.cluster", "osmasem")))
-    stop("\"object\" must be an object of neither class \"meta\", \"meta3X\", \"wls\", \"reml\", \"tssem1FEM\", \"tssem1REM\", \"tssem1FEM.cluster\", \"wls.cluster\" or \"osmasem\".")
+  if (!is.element(class(object)[1], c("wls", "tssem1FEM", "tssem1REM", "meta", "meta3X", "reml", "tssem1FEM.cluster", "wls.cluster", "osmasem", "MxModel")))
+    stop("\"object\" must be an object of neither class \"meta\", \"meta3X\", \"wls\", \"reml\", \"tssem1FEM\", \"tssem1REM\", \"tssem1FEM.cluster\", \"wls.cluster\", \"osmasem\", or \"MxModel\".")
 
+  ## Cluster related objects
   if (is.element(class(object)[1], c("tssem1FEM.cluster", "wls.cluster"))) {
-    out <- lapply(object, rerun, ...)
-    class(out) <- class(object)[1]
+      out <- lapply(object, rerun, ...)
+      class(out) <- class(object)[1]
+  ## Pure MxModel objects    
+  } else if (is.element(class(object)[1], "MxModel")) {
+      out <- mxTryHard(object, greenOK=TRUE, paste=FALSE, bestInitsOutput=FALSE, ...)
+  ## Other metaSEM objects    
   } else {
     out <- object   
     # No LB option
