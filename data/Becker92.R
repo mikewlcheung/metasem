8c02bb3ecb8395065d0f78771f4d1a50cd38619d
Mike Cheung <mikewlcheung@nus.edu.sg>
2020-06-13 12:04:46 +0800

    Changed variable names in Becker92 and Becker94

--- data/Becker92.R
+++ data/Becker92.R
@@ -2,8 +2,8 @@ r <- matrix(c(.46,.31,.19,.46,.55,.32,.397,.402,.183,.266,.567,.218,.28,.19,.18,
 my.df <- lapply(split(r, 1:6), function(x) { out <- diag(rep(1,3))
                                              out[lower.tri(out)] <- x
                                              out[upper.tri(out)] <- x
-                                             dimnames(out) <- list(c("Math_aptitude", "Spatial_ability", "Verbal_ability"),
-                                                                   c("Math_aptitude", "Spatial_ability", "Verbal_ability"))
+                                             dimnames(out) <- list(c("Math", "Spatial", "Verbal"),
+                                                                   c("Math", "Spatial", "Verbal"))
                                              out})
 names(my.df) <- c("Berry (1957)", "Rosenberg (1981)", "Weiner 1 (1984)", "Weiner 2 (1984)", "Becker 1 (1978)", "Becker 2 (1978)")
 Becker92 <- list(data=my.df, n=c(103,69,69,70,153,74))   
