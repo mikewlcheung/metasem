797f8526c433688942df44bcdaa7c0686db2500d
Mike Cheung <mikewlcheung@nus.edu.sg>
2020-06-24 16:52:58 +0800

    Added extraTries in rerun

--- R/rerun.R
+++ R/rerun.R
@@ -1,4 +1,4 @@
-rerun <- function(object, autofixtau2=FALSE, ...) {
+rerun <- function(object, autofixtau2=FALSE, extraTries=10, ...) {
     if (!is.element(class(object)[1], c("wls", "tssem1FEM", "tssem1REM", "meta", "meta3X", "reml",
                                         "tssem1FEM.cluster", "wls.cluster", "osmasem", "MxModel")))
     stop("\"object\" must be an object of neither class \"meta\", \"meta3X\", \"wls\",
@@ -7,7 +7,7 @@ rerun <- function(object, autofixtau2=FALSE, ...) {
     ## Run a rerun without autofixtau2 to minimize over-fixing
     ## Many of the NA in SEs may disappear after rerunning it.
     if (autofixtau2 & is.element(class(object)[1], c("tssem1REM", "meta", "osmasem"))) {
-        object <- rerun(object, autofixtau2=FALSE, ...)
+        object <- rerun(object, autofixtau2=FALSE, extraTries=extraTries, ...)
     }
     
     ## Automatically fix the problematic Tau2 into 0 for tssem1REM and meta objects
@@ -51,21 +51,21 @@ rerun <- function(object, autofixtau2=FALSE, ...) {
   
     ## Cluster related objects
     if (is.element(class(object)[1], c("tssem1FEM.cluster", "wls.cluster"))) {
-        out <- lapply(object, rerun, ...)
+        out <- lapply(object, rerun, extraTries=extraTries, ...)
         class(out) <- class(object)[1]
         ## Pure MxModel objects    
     } else if (is.element(class(object)[1], "MxModel")) {
-        out <- mxTryHard(object, greenOK=TRUE, paste=FALSE, bestInitsOutput=FALSE, ...)
+        out <- mxTryHard(object, extraTries=extraTries, greenOK=TRUE, paste=FALSE, bestInitsOutput=FALSE, ...)
         ## Other metaSEM objects    
     } else {
         out <- object   
         ## No LB option
         if (is.null(object$intervals.type)) {
-            out$mx.fit <- mxTryHard(object$mx.fit, greenOK=TRUE, paste=FALSE, bestInitsOutput=FALSE, ...)
+            out$mx.fit <- mxTryHard(object$mx.fit, extraTries=extraTries, greenOK=TRUE, paste=FALSE, bestInitsOutput=FALSE, ...)
         } else {
             switch(object$intervals.type,
-                   z = out$mx.fit <- mxTryHard(object$mx.fit, greenOK=TRUE, paste=FALSE, bestInitsOutput=FALSE, ...),
-                   LB =out$mx.fit <- mxTryHard(object$mx.fit, greenOK=TRUE, paste=FALSE, bestInitsOutput=FALSE,
+                   z = out$mx.fit <- mxTryHard(object$mx.fit, extraTries=extraTries, greenOK=TRUE, paste=FALSE, bestInitsOutput=FALSE, ...),
+                   LB =out$mx.fit <- mxTryHard(object$mx.fit, extraTries=extraTries, greenOK=TRUE, paste=FALSE, bestInitsOutput=FALSE,
                                                intervals=TRUE, ...))
         }
     }    
