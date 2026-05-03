9c35804b62dac1a57bfd2bb39c660f92874dce5e
Mike Cheung <mikewlcheung@nus.edu.sg>
2026-05-03 13:52:40 +0800

    Correct minor bugs in

--- R/rerun.R
+++ R/rerun.R
@@ -1,8 +1,8 @@
 #' Rerun models via mxTryHard()
-#' 
+#'
 #' It reruns models via mxTryHard().
-#' 
-#' 
+#'
+#'
 #' @param object An object of either class \code{tssem1FEM}, class
 #' \code{tssem1REM}, class \code{wls}, class \code{meta}, class \code{reml},
 #' class \code{osmasem}, class \code{osmasem3L}, and class \code{MxModel}.
@@ -16,22 +16,22 @@
 #' @author Mike W.-L. Cheung <mikewlcheung@@nus.edu.sg>
 #' @keywords tssem meta osmasem osmasem3L wls
 #' @examples
-#' 
+#'
 #' \donttest{
 #' random1 <- tssem1(Digman97$data, Digman97$n, method="REM", RE.type="Diag")
 #' random1_rerun <- rerun(random1)
 #' summary(random1_rerun)
 #' }
-#' 
+#'
 rerun <- function(object, autofixtau2=FALSE, extraTries=10, ...) {
   if (!is.element(class(object)[1], c("wls", "tssem1FEM", "tssem1REM", "meta",
                                       "meta3LFIML", "reml",
                                       "tssem1FEM.cluster", "wls.cluster",
                                       "osmasem", "osmasem2", "osmasem3L",
-                                      "MxModel", "sem")))
+                                      "MxModel", "mxsem")))
     stop("'object' must be an object of neither class 'meta', 'meta3LFIML',
 'wls', 'reml', 'tssem1FEM', 'tssem1REM', 'tssem1FEM.cluster', 'wls.cluster',
-'osmasem', 'osmasem2', 'osmasem3L', 'MxModel', or 'mxRAMModel'.")
+'osmasem', 'osmasem2', 'osmasem3L', 'MxModel', or 'mxsem'.")
 
   ## Run a rerun without autofixtau2 to minimize over-fixing
   ## Many of the NA in SEs may disappear after rerunning it.
@@ -39,85 +39,56 @@ rerun <- function(object, autofixtau2=FALSE, extraTries=10, ...) {
                                                    "osmasem", "osmasem2"))) {
     object <- rerun(object, autofixtau2=FALSE, extraTries=extraTries, ...)
   }
-  
+
   ## Automatically fix the problematic Tau2 into 0 for tssem1REM and meta objects
   ## osmasem2 object is similar to meta object as it uses Tau2
   if (autofixtau2 & is.element(class(object)[1], c("tssem1REM", "meta",
                                                    "osmasem2"))) {
-    ## Get the Tau2 with NA is SE
-    tau2nan <- suppressWarnings(sqrt(diag(vcov(object, select="random"))))
-    
-    ## Check if tau2nan exists; otherwise names(tau2nan) returns error        
-    if (any(tau2nan <- is.nan(tau2nan))) {
-      ## Keep elements with NAN (TRUE)
-      tau2nan <- tau2nan[tau2nan]
-      ## Fix the Tau2 at 0
-      object$mx.fit <- omxSetParameters(object$mx.fit, names(tau2nan),
+    se_tau2 <- suppressWarnings(sqrt(diag(vcov(object, select="random"))))
+    nan_idx <- is.nan(se_tau2)
+    if (any(nan_idx)) {
+      object$mx.fit <- omxSetParameters(object$mx.fit, names(se_tau2)[nan_idx],
                                         free=FALSE, values=0)
     }
   }
 
   ## Automatically fix the problematic Tau2 into 0 for osmasem objects
-  if (autofixtau2 & is.element(class(object)[1], "osmasem")) {
-    ## Check if there are negative variances
-    tau2nan <- suppressWarnings(sqrt(diag(vcov(object, select="random"))))
+  if (autofixtau2 & class(object)[1] == "osmasem") {
+    se_tau2 <- suppressWarnings(sqrt(diag(vcov(object, select="random"))))
+    nan_idx <- is.nan(se_tau2)
 
-    if (any(tau2nan <- is.nan(tau2nan))) {
-      tau2nan <- tau2nan[tau2nan]
-      
-      ## FIXME: Need to check how robust it is for "Symm" and "User"
-      ## Dirty fix to check whether the transformation is expLog or sqSD
-      ## Remove all white spaces
-      my.transform <- gsub("\\s", "", deparse(object$mx.fit$Tau2$formula)) 
-      if (grep("exp", my.transform)) {
-        ## exp(-Inf)=0
-        values = -Inf
-        ## Assuming sdSD
+    if (any(nan_idx)) {
+      ## Detect the transformation used in Tau2 (expLog uses exp(); sqSD uses squaring)
+      my.transform <- gsub("\\s", "", deparse(object$mx.fit$Tau2$formula))
+      if (grepl("exp", my.transform)) {
+        ## exp(-Inf) = 0
+        values <- -Inf
       } else {
-        ## 0^2=0
-        values = 0
+        ## 0^2 = 0
+        values <- 0
       }
-      object$mx.fit <- omxSetParameters(object$mx.fit, names(tau2nan),
+      object$mx.fit <- omxSetParameters(object$mx.fit, names(se_tau2)[nan_idx],
                                         free=FALSE, values=values)
     }
-  }    
-   
+  }
+
   ## Cluster related objects
   if (is.element(class(object)[1], c("tssem1FEM.cluster", "wls.cluster"))) {
     out <- lapply(object, rerun, extraTries=extraTries, ...)
     class(out) <- class(object)[1]
-    ## Pure MxModel objects    
-  } else if (is.element(class(object)[1], "MxModel")) {
+    ## Pure MxModel objects
+  } else if (class(object)[1] == "MxModel") {
     out <- mxTryHard(object, extraTries=extraTries, greenOK=TRUE, paste=FALSE,
                      bestInitsOutput=FALSE, ...)
-    ## Other metaSEM objects    
+    ## Other metaSEM objects
   } else {
-
     out <- object
     object$mx.fit <- mxOption(object$mx.fit, "Calculate Hessian", "Yes")
     object$mx.fit <- mxOption(object$mx.fit, "Standard Errors", "Yes")
-    
-    ## No LB option
-    if (is.null(object$intervals.type)) {
-      out$mx.fit <- mxTryHard(object$mx.fit, extraTries=extraTries,
-                              greenOK=TRUE, paste=FALSE, bestInitsOutput=FALSE,
-                              ...)
-    } else {
-      switch(object$intervals.type,
-             z = out$mx.fit <- mxTryHard(object$mx.fit, extraTries=extraTries,
-                                         greenOK=TRUE, paste=FALSE,
-                                         bestInitsOutput=FALSE, ...),
-             LB =out$mx.fit <- mxTryHard(object$mx.fit, extraTries=extraTries,
-                                         greenOK=TRUE, paste=FALSE,
-                                         bestInitsOutput=FALSE,
-                                         intervals=TRUE, ...))
-    }
+    out$mx.fit <- mxTryHard(object$mx.fit, extraTries=extraTries,
+                            greenOK=TRUE, paste=FALSE, bestInitsOutput=FALSE,
+                            intervals = isTRUE(object$intervals.type == "LB"),
+                            ...)
   }
-  ## Run it again as the SEs sometimes disappear
-  ## fit <- out$mx.fit
-  ## fit <- mxOption(fit, "Calculate Hessian", "Yes")
-  ## fit <- mxOption(fit, "Standard Errors", "Yes")
-  ## out$mx.fit <- mxRun(fit, silent=TRUE, intervals=(intervals.type=="LB"),
-  ##                     suppressWarnings=TRUE)
   out
 }
